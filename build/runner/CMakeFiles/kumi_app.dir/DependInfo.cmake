7df12e2592b30e282568275e0e17840a1c9db7ab
HarshYadav1711 <harshyadav8118mpvm@gmail.com>
2026-02-23 00:01:55 +0530

    fix(CMake and Java version): Fixed some CMake and Java version mismatch errors

---
+++ build/runner/CMakeFiles/kumi_app.dir/DependInfo.cmake
@@ -0,0 +1,53 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "RC"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_RC
+  "D:/Fun/Kumi Labs/app/windows/runner/Runner.rc" "D:/Fun/Kumi Labs/build/runner/CMakeFiles/kumi_app.dir/Runner.rc.obj"
+  )
+
+# Preprocessor definitions for this target.
+set(CMAKE_TARGET_DEFINITIONS_RC
+  "FLUTTER_VERSION=\"1.0.0+1\""
+  "FLUTTER_VERSION_BUILD=1"
+  "FLUTTER_VERSION_MAJOR=1"
+  "FLUTTER_VERSION_MINOR=0"
+  "FLUTTER_VERSION_PATCH=0"
+  "NOMINMAX"
+  "UNICODE"
+  "_DEBUG"
+  "_HAS_EXCEPTIONS=0"
+  "_UNICODE"
+  )
+
+# The include file search paths:
+set(CMAKE_RC_TARGET_INCLUDE_PATH
+  "D:/Fun/Kumi Labs/app/windows"
+  "D:/Fun/Kumi Labs/app/windows/flutter/ephemeral"
+  "D:/Fun/Kumi Labs/app/windows/flutter/ephemeral/cpp_client_wrapper/include"
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "D:/Fun/Kumi Labs/app/windows/flutter/generated_plugin_registrant.cc" "runner/CMakeFiles/kumi_app.dir/__/flutter/generated_plugin_registrant.cc.obj" "gcc" "runner/CMakeFiles/kumi_app.dir/__/flutter/generated_plugin_registrant.cc.obj.d"
+  "D:/Fun/Kumi Labs/app/windows/runner/flutter_window.cpp" "runner/CMakeFiles/kumi_app.dir/flutter_window.cpp.obj" "gcc" "runner/CMakeFiles/kumi_app.dir/flutter_window.cpp.obj.d"
+  "D:/Fun/Kumi Labs/app/windows/runner/main.cpp" "runner/CMakeFiles/kumi_app.dir/main.cpp.obj" "gcc" "runner/CMakeFiles/kumi_app.dir/main.cpp.obj.d"
+  "D:/Fun/Kumi Labs/app/windows/runner/utils.cpp" "runner/CMakeFiles/kumi_app.dir/utils.cpp.obj" "gcc" "runner/CMakeFiles/kumi_app.dir/utils.cpp.obj.d"
+  "D:/Fun/Kumi Labs/app/windows/runner/win32_window.cpp" "runner/CMakeFiles/kumi_app.dir/win32_window.cpp.obj" "gcc" "runner/CMakeFiles/kumi_app.dir/win32_window.cpp.obj.d"
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_LINKED_INFO_FILES
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_FORWARD_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ build/runner/CMakeFiles/kumi_app.dir/cmake_clean.cmake
@@ -0,0 +1,22 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/kumi_app.dir/Runner.rc.obj"
+  "CMakeFiles/kumi_app.dir/__/flutter/generated_plugin_registrant.cc.obj"
+  "CMakeFiles/kumi_app.dir/__/flutter/generated_plugin_registrant.cc.obj.d"
+  "CMakeFiles/kumi_app.dir/flutter_window.cpp.obj"
+  "CMakeFiles/kumi_app.dir/flutter_window.cpp.obj.d"
+  "CMakeFiles/kumi_app.dir/main.cpp.obj"
+  "CMakeFiles/kumi_app.dir/main.cpp.obj.d"
+  "CMakeFiles/kumi_app.dir/utils.cpp.obj"
+  "CMakeFiles/kumi_app.dir/utils.cpp.obj.d"
+  "CMakeFiles/kumi_app.dir/win32_window.cpp.obj"
+  "CMakeFiles/kumi_app.dir/win32_window.cpp.obj.d"
+  "kumi_app.exe"
+  "kumi_app.exe.manifest"
+  "kumi_app.pdb"
+  "libkumi_app.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX RC)
+  include(CMakeFiles/kumi_app.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
